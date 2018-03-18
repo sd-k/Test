ee33de81f901f4da45c13e67c88cd227f0cb072c
Sougata <sddcst2014@gmal.com>
2018-03-18 18:12:28 +0530

    text modified

--- test_git.js
+++ test_git.js
@@ -1,4 +1,4 @@
 console.log("Hi ! I am Mr. Ass");
 console.log(
-	"This is a test to create and delete a repository and cloning and pushing files using CLI"
+	"This is a test to create/delete a repository and cloning/pushing files in a repository using Command Line Interface"
 );
